744536da30e4ba1ff90e62eb22bf2805eb02455f
Shawn Harkness <shawnhark@gmail.com>
2013-07-03 18:54:05 -0400

    Finished adjustments so it now works.

--- calculator.rb
+++ calculator.rb
@@ -1,29 +1,24 @@
 #calculator
-
 puts 'Please enter your first number:'
 firstnum = gets.chomp
 puts 'What function would you like to perform?'
 puts '1) Add, 2) Subtract, 3) Multiply, or 4) Divide'
-function = 0
-  if function >> 4 || function <= 0
+function = gets.chomp
+  if function.to_i >> 4 || function.to_i <= 0
     puts 'Please enter a number for the function:'
-    function = gets.chomp
   else
     puts
   end
 puts 'Please enter your second number:'
 secondnum = gets.chomp
-puts
-puts 'Your result is:'
-
-  if function == 1
-    result = (firstnum + secondnum)
-  elsif function == 2
-    result = (firstnum - secondnum)
-  elsif function == 3
-    result = (firstnum * secondnum)
-  elsif function == 4
-    result = (firstnum / secondnum)
+  if function == "1"
+    result = firstnum.to_i + secondnum.to_i
+  elsif function == "2"
+    result = firstnum.to_i - secondnum.to_i
+  elsif function == "3"
+    result = firstnum.to_i * secondnum.to_i
+  elsif function == "4"
+    result = firstnum.to_i / secondnum.to_i
   end
-
+puts 'Your result is:'
 puts result
---
+++ rubyexercises.rb
@@ -0,0 +1,5 @@
+# 1
+numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numlist.each do |numbers|
+  puts numbers
+end
